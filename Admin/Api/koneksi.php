ef2fb743dfe3f5e3b496af53a6e2626e0abb1d5b
jainudin <jainudin.perdana@gmail.com>
2021-07-27 15:09:48 +0700

    countdown

--- Admin/Api/koneksi.php
+++ Admin/Api/koneksi.php
@@ -2,11 +2,11 @@
 //KONVERSI PHP KE PHP 7
 require_once "parser-php-version.php";
 
-//$mysql_host = "localhost";
-$mysql_host = "localhost:50000";
+$mysql_host = "localhost";
+//$mysql_host = "localhost:50000";
 $mysql_user = "root";
-//$mysql_pass = "";
-$mysql_pass = "GL-System123";
+$mysql_pass = "";
+//$mysql_pass = "GL-System123";
 $mysql_dbname = "wedding-v1";
 
 //Api key diganti lagi karena saldo habis
